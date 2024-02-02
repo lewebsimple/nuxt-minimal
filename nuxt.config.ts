500e4287601c78edbe24dce649f1a1a9846c8426
Pascal Martineau <pascal@lewebsimple.ca>
2024-02-02 08:46:28 -0500

    feat: svg components support

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,11 @@ import { description } from "./package.json";
 
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true },
+  components: [
+    { path: "~/components", pathPrefix: false },
+    { path: "~/assets/svg", extensions: ["svg"], prefix: "Svg" },
+  ],
+  modules: ["@lewebsimple/nuxt3-svg"],
   runtimeConfig: {
     public: {
       siteName: process.env.NUXT_PUBLIC_SITE_NAME || description,
