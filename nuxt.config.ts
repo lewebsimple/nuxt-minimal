0e4f90893b18acdcdd81f4d9f80bafb28b32eb6c
Pascal Martineau <pascal@lewebsimple.ca>
2024-02-26 09:43:07 -0500

    refactor: Initial app/ layer

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,16 +1,5 @@
-import { description } from "./package.json";
-
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  components: [
-    { path: "~/components", pathPrefix: false },
-    { path: "~/assets/svg", extensions: ["svg"], prefix: "Svg" },
-  ],
-  modules: ["@lewebsimple/nuxt3-svg"],
-  runtimeConfig: {
-    public: {
-      siteName: process.env.NUXT_PUBLIC_SITE_NAME || description,
-    },
-  },
+  extends: ["./app"],
   typescript: { typeCheck: true },
 });
